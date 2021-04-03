9d0aa3cccaa9a0f1df486dfcab0bd5cab53ce462
David Headrick <davidheadrick1@gmail.com>
2021-04-03 01:36:48 -0400

    david initial commit

--- cipher03.h
+++ cipher03.h
@@ -5,17 +5,17 @@
 ********************************************************************/
 #ifndef CIPHER03_H
 #define CIPHER03_H
-#include "cipher.h"
+
 /********************************************************************
  * CLASS
  *******************************************************************/
 class Cipher03 : public Cipher
 {
 public:
-   virtual std::string getPseudoAuth()  { return "pseudocode author"; }
-   virtual std::string getCipherName()  { return "cipher name"; }
-   virtual std::string getEncryptAuth() { return "encrypt author"; }
-   virtual std::string getDecryptAuth() { return "decrypt author"; }
+   virtual std::string getPseudoAuth()  { return "David Headrick"; }
+   virtual std::string getCipherName()  { return "Autokey Cipher"; }
+   virtual std::string getEncryptAuth() { return "David Headrick"; }
+   virtual std::string getDecryptAuth() { return "David Headrick"; }
 
    /***********************************************************
     * GET CIPHER CITATION
@@ -23,7 +23,21 @@ public:
     ***********************************************************/
    virtual std::string getCipherCitation()
    {
-      return std::string("citation");
+      std::string s;
+      s =  "UCF.edu, ";
+      s += "\'3.2 Autokey Cipher\', \n    retrieved: ";
+      s += "http://www.cs.ucf.edu/~dmarino/ucf/cis3362/lectures/3-PolyAlpha.pdf\n";
+
+      s +=  "GeeksforGeeks, ";
+      s += "\'Autokey Cipher | Symmetric Ciphers\', \n    retrieved: ";
+      s += "https://www.geeksforgeeks.org/autokey-cipher-symmetric-ciphers/\n";
+
+      s +=  "OpenGenus.org, ";
+      s += "\'Auto Key Cipher\', \n    retrieved: ";
+      s += "https://iq.opengenus.org/auto-key-cipher/\n";
+
+
+      return s;
    }
    
    /**********************************************************
@@ -36,10 +50,20 @@ public:
 
       // TODO: please format your pseudocode
       // The encrypt pseudocode
-      str =  "insert the encryption pseudocode\n";
+      str =  "\n   encrypt(plainText, password)\n\n";
+      str += "      FOR (i=0; i<k.length; i++)\n";
+      str += "         c[i] = p[i] + k[i];\n";
+      str += "      FOR (j=k.length; j<p.length; j++)\n";
+      str += "         c[j] = p[j] + p[j-k.length];\n";
+      str += "      RETURN cipherText\n";
 
       // The decrypt pseudocode
-      str += "insert the decryption pseudocode\n";
+      str += "\n   decrypt(cipherText, password)\n\n";
+      str += "      FOR (i=0; i<k.length; i++)\n";
+      str += "         c[i] = p[i] + k[i];\n";
+      str += "      FOR (j=k.length; j<p.length; j++)\n";
+      str += "         c[j] = p[j] + p[j-k.length];\n";
+      str += "      RETURN cipherText\n";
 
       return str;
    }
@@ -48,11 +72,41 @@ public:
     * ENCRYPT
     * TODO: ADD description
     **********************************************************/
-   virtual std::string encrypt(const std::string & plainText,
+   virtual std::string encrypt(const std::string & plainText, 
                                const std::string & password)
    {
-      std::string cipherText = plainText;
-      // TODO - Add your code here
+      std::string cipherText = "";
+      std::vector<int> keystream;
+      int accum = 0;
+
+      // key maker function return a integer key
+      for (int i = 0; i < password.length(); i++)
+      {
+         char c = password[i];
+         accum += (int)c;
+      }
+      int key = ((accum + password.length()) % 128); // mod it by 128 ASCII size of table, could be const
+
+// 1. push_back the key onto the vector
+      keystream.push_back(key);
+
+// 2. go from 0 to plaintext.length() - 2
+//    turn into char c = plaintext[i]
+//    keystream.push_back((int)c)
+      for(int i = 0; i <= (plainText.length() - 2); i++)
+      {
+         char c = plainText[i];
+         keystream.push_back((int)c); // create the keystream (integer version (ascii value) of the char on each element of the vector)
+      }
+
+// 3. loop thru plaintext, with every element of plaintext, add it to the same element of keystream, then mod it with 128
+      for(int i = 0; i <= plainText.length(); i++)
+      {
+         char c = plainText[i]; // turn element into char
+         cipherText.push_back((char)((keystream[i] + ((int)c)) % 128));
+
+      }
+
       return cipherText;
    }
 
@@ -60,11 +114,39 @@ public:
     * DECRYPT
     * TODO: ADD description
     **********************************************************/
-   virtual std::string decrypt(const std::string & cipherText,
+   virtual std::string decrypt(const std::string & cipherText, 
                                const std::string & password)
    {
-      std::string plainText = cipherText;
-      // TODO - Add your code here
+      // std::string plainText = cipherText;
+      std::string plainText = "";
+      std::vector<int> keystream;
+      int accum = 0;
+
+      // key maker function return a integer key
+      for (int i = 0; i < password.length(); i++)
+      {
+         char c = password[i];
+         accum += (int)c;
+      }
+      int key = ((accum + password.length()) % 128); // mod it by 128 ASCII size of table, could be const
+
+      for(int i = 1; i < cipherText.length(); i++)
+      {
+         // for every element turn it into a char, then turn into an int, then push_Back onto keystream
+         char c = cipherText[i];
+         keystream.push_back((int)c);
+      }
+      keystream.push_back(key); // represents the ciphertext and key
+
+      // convert from int to char
+      for(int i = 0; i <= cipherText.length(); i++)
+      {
+         char c = cipherText[i];
+         int intAscii = ((int)c - keystream[i] + 128) % 128;
+         plainText.push_back((char)intAscii); // caste back to char to display human readable
+      }
+
+
       return plainText;
    }
 };
